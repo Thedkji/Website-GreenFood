d4c135230a2c6a25c477d46a21106ea455cd3de1
Thedkji <nmquang2303@gmail.com>
2024-10-08 19:26:34 +0700

    Xây dựng layout client

---
+++ resources/views/clients/layouts/components/script.blade.php
@@ -0,0 +1,10 @@
+ <!-- JavaScript Libraries -->
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+ <script src="{{ env('VIEW_CLIENT') }}/lib/easing/easing.min.js"></script>
+ <script src="{{ env('VIEW_CLIENT') }}/lib/waypoints/waypoints.min.js"></script>
+ <script src="{{ env('VIEW_CLIENT') }}/lib/lightbox/js/lightbox.min.js"></script>
+ <script src="{{ env('VIEW_CLIENT') }}/lib/owlcarousel/owl.carousel.min.js"></script>
+
+ <!-- Template Javascript -->
+ <script src="{{ env('VIEW_CLIENT') }}/js/main.js"></script>
