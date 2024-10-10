29a29687c515719089759f7e48ec831540b4880e
CuongTran279 <cuongtdph32393@fpt.edu.vn>
2024-10-11 00:28:16 +0700

    [Cường] Cắt giao diện sản phẩm (Có validate và preview ảnh)

---
+++ app/Http/Controllers/admins/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\admins;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('admins.products.list-product');
+    }
+
+    public function create()
+    {
+        return view('admins.products.add-product');
+    }
+}
