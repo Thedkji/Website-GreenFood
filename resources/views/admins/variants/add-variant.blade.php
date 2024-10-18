fd88d609048f67d2a7ff527bd7f0640aa101b9b2
Thedkji <nmquang2303@gmail.com>
2024-10-18 15:22:33 +0700

    [Quang] Sửa lại tên và cấu trúc variant cho Cường

--- resources/views/admins/variants/add-variant.blade.php
+++ resources/views/admins/variants/add-variant.blade.php
@@ -4,42 +4,48 @@
 
 @section('start-page-title', 'Thêm mới biến thể')
 
-@section('content')
-@if (session('success'))
-<div class="alert alert-success">
-    {{ session('success') }}
-</div>
-@endif
+@section('link')
+    <li class="breadcrumb-item"><a href="{{ route('admin.variants.variants.index') }}">Biến thể</a></li>
+    <li class="breadcrumb-item active">Thêm mới biến thể</li>
+@endsection
 
-@if (session('error'))
-<div class="alert alert-danger">
-    {{ session('error') }}
-</div>
-@endif
-<form class="needs-validation" novalidate method="post" action="{{ route('admin.variants.variants.store') }}">
-    @csrf
-    @method('POST')
-    <div class="row">
-        <div class="col-lg-8">
-            <div class="mb-3">
-                <label for="validationCustom01" class="form-label">Tên biến thể</label>
-                <input type="text" class="form-control" id="validationCustom01" name="name" value="{{old('name')}}" required>
-                @error('name')
-                <div class="invalid-feedback">
-                    {{ $message }}
+@section('content')
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
+    <form class="needs-validation" novalidate method="post" action="{{ route('admin.variants.variants.store') }}">
+        @csrf
+        @method('POST')
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="mb-3">
+                    <label for="validationCustom01" class="form-label">Tên biến thể</label>
+                    <input type="text" class="form-control" id="validationCustom01" name="name"
+                        value="{{ old('name') }}" required>
+                    @error('name')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                    <div class="valid-feedback">
+                        Hợp lệ!
+                    </div>
+                    <div class="invalid-feedback">
+                        Vui lòng không để trống
+                    </div>
                 </div>
-                @enderror
-                <div class="valid-feedback">
-                    Hợp lệ!
+                <div class="col-12 mb-3">
+                    <button class="btn btn-secondary" type="submit">Thêm mới</button>
                 </div>
-                <div class="invalid-feedback">
-                    Vui lòng không để trống
-                </div>
-            </div>
-            <div class="col-12 mb-3">
-                <button class="btn btn-secondary" type="submit">Thêm mới</button>
             </div>
         </div>
-    </div>
-</form>
+    </form>
 @endsection
