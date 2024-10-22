f1000f5305ca0ef1849f065e0e16a2692a6af149
Kienltph38042 <kienltph38042@fpt.edu.vn>
2024-10-22 22:00:16 +0700

    [Kiên] Cập nhận trang quản lý bình luận

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\admins\CommentController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,6 +27,7 @@ Route::prefix('admin')
         require_once base_path("routes/admins/product.php");
         require_once base_path("routes/admins/variant.php");
         require_once base_path("routes/admins/order.php");
+        require_once base_path("routes/admins/comment.php");
     });
 
 // Client
@@ -44,3 +45,4 @@ Route::prefix('client')
         require_once base_path("routes/clients/error.php");
         require_once base_path("routes/clients/message.php");
     });
+ 
