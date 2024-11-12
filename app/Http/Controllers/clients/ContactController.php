9d2967be1c278d8c17e96b1845437ec68fe64201
Phuongthiphuongthi <phuongpato@gmail.com>
2024-11-12 16:30:01 +0700

    [Phương]Sửa danh mục sản phẩm

--- app/Http/Controllers/clients/ContactController.php
+++ app/Http/Controllers/clients/ContactController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\clients;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Mailable;
+use Mail;
 
 class ContactController extends Controller
 {
     public function contact(){
+        $email = 'greenfood8386@gmail.com';
+        // Mail::to('$email')->send(new ContactEmail());
         return view('clients.contacts.contact');
     }
 }
