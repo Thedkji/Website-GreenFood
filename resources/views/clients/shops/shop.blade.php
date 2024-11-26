c032ed063227ea3cc97b96b02e347b684f94f7dc
NMQuang233 <nmquang2303@gmail.com>
2024-11-26 17:19:12 +0700

    [Quang] Cập nhật lại giao diện trang sản phẩm

--- resources/views/clients/shops/shop.blade.php
+++ resources/views/clients/shops/shop.blade.php
@@ -19,7 +19,7 @@
 @section('content')
 
     <!-- Fruits Shop Start-->
-    <div class="container-fluid py-5">
+    <div class="container-fluid fruite py-5">
         <div class="container py-5">
             @include('clients.shops.side-bar')
 
