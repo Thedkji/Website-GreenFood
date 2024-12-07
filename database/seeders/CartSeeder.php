1c97afd37500ca2e7b3a1f9ab72938d46c681e05
Quang <nmquang2303@gmail.com>
2024-12-07 14:31:02 +0700

    [Quang] Cập nhật seeder và fix 1 số lỗi

--- database/seeders/CartSeeder.php
+++ database/seeders/CartSeeder.php
@@ -15,11 +15,12 @@ class CartSeeder extends Seeder
     public function run(): void
     {
         $carts = array(
-            array('id' => '21', 'product_id' => '2', 'user_id' => '1', 'sku' => 'SPBT800530', 'quantity' => '3', 'created_at' => '2024-12-03 11:40:58', 'updated_at' => '2024-12-03 11:40:58'),
-            array('id' => '22', 'product_id' => '9', 'user_id' => '1', 'sku' => 'SPBT945718', 'quantity' => '3', 'created_at' => '2024-12-03 11:41:06', 'updated_at' => '2024-12-03 11:41:06'),
-            array('id' => '23', 'product_id' => '8', 'user_id' => '1', 'sku' => 'SP160564', 'quantity' => '2', 'created_at' => '2024-12-03 11:41:15', 'updated_at' => '2024-12-03 11:41:15'),
             array('id' => '28', 'product_id' => '15', 'user_id' => '2', 'sku' => 'SPBT660996', 'quantity' => '1', 'created_at' => '2024-12-04 13:54:54', 'updated_at' => '2024-12-04 13:54:54'),
-            array('id' => '29', 'product_id' => '15', 'user_id' => '2', 'sku' => 'SPBT814589', 'quantity' => '1', 'created_at' => '2024-12-04 13:54:58', 'updated_at' => '2024-12-04 13:54:58')
+            array('id' => '29', 'product_id' => '15', 'user_id' => '2', 'sku' => 'SPBT814589', 'quantity' => '1', 'created_at' => '2024-12-04 13:54:58', 'updated_at' => '2024-12-04 13:54:58'),
+            array('id' => '42', 'product_id' => '5', 'user_id' => '1', 'sku' => 'SPBT276579', 'quantity' => '1', 'created_at' => '2024-12-07 13:51:02', 'updated_at' => '2024-12-07 13:51:02'),
+            array('id' => '43', 'product_id' => '5', 'user_id' => '1', 'sku' => 'SPBT491526', 'quantity' => '3', 'created_at' => '2024-12-07 13:51:07', 'updated_at' => '2024-12-07 13:51:07'),
+            array('id' => '44', 'product_id' => '2', 'user_id' => '1', 'sku' => 'SPBT490156', 'quantity' => '2', 'created_at' => '2024-12-07 13:51:11', 'updated_at' => '2024-12-07 13:51:11'),
+            array('id' => '45', 'product_id' => '6', 'user_id' => '1', 'sku' => 'SPBT235883', 'quantity' => '3', 'created_at' => '2024-12-07 13:51:17', 'updated_at' => '2024-12-07 13:51:17')
         );
 
         foreach ($carts as $cart) {
